a9b2b0b32c6bc81f0486b13965d58450e23874de
Projjal Gop <gopprojjal@gmail.com>
2021-01-01 12:54:09 +0530

    Add files via upload

---
+++ helloworld-cuda.cu
@@ -0,0 +1,17 @@
+/*
+Problem Statement : Hello World in Cuda
+Author : Projjal Gop
+*/
+
+
+#include<stdio.h>
+
+__global__ void mykernel(void)
+{}
+
+int main(void)
+{
+	mykernel<<<1,1>>>();
+	printf("Hello World!\n");
+	return 0;
+}
